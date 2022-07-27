5b75d24e26d155ea90ffecdac540b69864b7828f
articos11 <viniciuscaldas0003@gmail.com>
2022-07-27 14:05:00 -0300

    new elements

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const lights = document.querySelector('.light-source');
 const container = document.querySelector('#m-container');
 const links = document.querySelector('nav');
 const socials = document.querySelector('.a-container');
-const title = document.querySelector('.page-title')
-const left = document.querySelector('#left-content')
+const title = document.querySelector('.page-title');
+const left = document.querySelector('#left-content');
+const middle = document.querySelector('.adventure-title');
+const text = document.querySelector('.text');
 
 // Not sure if this is the most efficient way, but I used a one-on-one selector to give a 'toggle' class called 'active' to every class selected above. With this, whenever I click on the 'sun' imagem, it will change to the .active class in CSS. 
 function modeswitch(){
@@ -13,4 +15,6 @@ function modeswitch(){
     socials.classList.toggle('active');
     left.classList.toggle('active');
     lights.classList.toggle('active');
+    middle.classList.toggle('active');
+    text.classList.toggle('active');
 }
